e39e477b9ec87e3ceff51e9acea0d9453d0c6ea2
Daniel Bogale <104236560+danibog@users.noreply.github.com>
2023-09-23 20:52:40 +0300

    formated the code

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,30 +1,27 @@
-import React from "react"
+import React from "react";
 const Hello = (props) => {
-
-  console.log(props)
+  console.log(props);
   return (
     <div>
       <p>
-
         Hello {props.name}, you are {props.age} years old
       </p>
     </div>
-  )
-}
+  );
+};
 
 const App = () => {
-
-  const name = 'Peter'
-  const age = 10
+  const name = "Peter";
+  const age = 10;
 
   return (
     <div>
       <h1>Greetings</h1>
 
-      <Hello name='Maya' age={26 + 10} />
+      <Hello name="Maya" age={26 + 10} />
       <Hello name={name} age={age} />
     </div>
-  )
-}
+  );
+};
 
 export default App;
