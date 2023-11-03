76fd3ad76b8def658eecba30fd9bc6c83ae13400
Daniel Bogale <104236560+danibog@users.noreply.github.com>
2023-11-03 14:13:18 +0300

    deleting phonebook will update render

--- part2/phonebook/src/components/Persons.jsx
+++ part2/phonebook/src/components/Persons.jsx
@@ -4,13 +4,13 @@ const Persons = ({ isFiltering, persons, filteredPersons, handleDeletion }) => {
   const personTobeRender = isFiltering ? filteredPersons : persons;
 
   return personTobeRender.map((person) => (
-    <div>
-      <span key={person.id}>
+    <div key={person.id}>
+      <span>
         {person.name} {person.number}
       </span>
       <button
         onClick={() => {
-          handleDeletion(person)
+          handleDeletion(person);
         }}
       >
         delete
