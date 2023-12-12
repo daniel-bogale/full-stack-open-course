546aa20dc700da13ebd2d6fa3176772822b2cb15
Daniel Bogale <104236560+danibog@users.noreply.github.com>
2023-12-12 17:05:07 +0300

    done exercise 3.3 - 3.6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.json());
 
-const phoneBooks = [
+let phoneBooks = [
   {
     id: 1,
     name: "Arto Hellas",
@@ -30,18 +30,85 @@ app.get("/", (request, response) => {
   response.send("<h1>Hello World</h1>");
 });
 
-app.get("/api/notes", (request, response) => {
+app.get("/api/persons", (request, response) => {
   response.json(phoneBooks);
 });
 
-app.get("/info", (req, res) => {
+app.get("/info", (request, response) => {
   const currentTime = new Date();
   const phoneBookLength = phoneBooks.length;
   console.log(phoneBookLength);
 
-  res.send(
+  response.send(
     `<p> Phonebook has info for ${phoneBookLength} people <br/> ${currentTime} </p>`
   );
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  console.log(id);
+  const phoneBook = phoneBooks.find((phoneBook) => phoneBook.id === id);
+  if (phoneBook) {
+    response.json(phoneBook);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  console.log(id);
+
+  const phoneBook = phoneBooks.find((phoneBook) => phoneBook.id === id);
+  if (phoneBook) {
+    phoneBooks = phoneBooks.filter((phoneBook) => phoneBook.id !== id);
+    //print
+    response.json(phoneBooks);
+    response.status(204).end();
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.post("/api/persons", (request, response) => {
+  const content = request.body;
+  if (!content) {
+    return response.status(400).json({ error: "content missing" });
+  }
+
+  if (!content.name || !content.number) {
+    return response.status(400).json({
+      error: content.name ? "phone number is missing" : "name is missing",
+    });
+  }
+
+  const nameExist = phoneBooks.find(
+    (phonebook) => phonebook.name === content.name
+  )
+    ? true
+    : false;
+
+  const phoneExist = phoneBooks.find(
+    (phonebook) => phonebook.number === content.number
+  )
+    ? true
+    : false;
+
+  if (phoneExist || nameExist) {
+    return response.status(400).json({
+      error: nameExist ? "name must be unique" : "phone number must be unique",
+    });
+  }
+
+  const randomId = Math.floor(1000 * Math.random());
+
+  const phonebook = {
+    ...content,
+    id: randomId,
+  };
+
+  phoneBooks.push(phonebook);
+  response.json(phonebook);
 });
 
 const PORT = 3001;
