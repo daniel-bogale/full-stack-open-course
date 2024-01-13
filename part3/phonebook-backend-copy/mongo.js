058f4443b67541aa86ecbd8e154d31594753f71a
Daniel Bogale <104236560+danibog@users.noreply.github.com>
2024-01-13 22:31:49 +0300

    fetched all objects on the notes collection

--- part3/phonebook-backend-copy/mongo.js
+++ part3/phonebook-backend-copy/mongo.js
@@ -11,16 +11,26 @@ mongoose.set("strictQuery", false);
 mongoose.connect(url);
 
 const noteSchema = new mongoose.Schema({
-  content: String,
+  content: {
+    type: String,
+    unique: true,
+  },
   important: Boolean,
 });
 
 const Note = new mongoose.model("Note", noteSchema);
 
-const note = new Note({ content: "Another content", important: true });
+// const note = new Note({ content: "HTML is easy", important: true });
 
-note.save().then((result) => {
-  console.log("note Saved!!");
-  console.log(result);
+// note.save().then((result) => {
+//   console.log("note Saved!!");
+//   console.log(result);
+//   mongoose.connection.close();
+// });
+
+Note.find({}).then((result) => {
+  result.forEach((note) => {
+    console.log(note);
+  });
   mongoose.connection.close();
 });
